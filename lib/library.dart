0cace88b3136089e77d9bb6ba46b3aa502f4ad8f
TrueCollaboration <pshonka.dev@gmail.com>
2023-12-13 02:25:48 +0100

    fix

--- lib/library.dart
+++ lib/library.dart
@@ -4,6 +4,8 @@ export 'package:bloc_common/library.dart' show
   IBlocEvent,
   IRepository,
   BaseRepository;
+  
+export 'package:bloc_common/src/external/events.dart';
 
 export 'src/external/interfaces/IWebBloc.dart';
 export 'src/external/base_web_bloc.dart';
--- lib/src/external/base_web_bloc.dart
+++ lib/src/external/base_web_bloc.dart
@@ -1,11 +1,18 @@
 import 'dart:html';
 
 import 'package:bloc_common/library.dart';
+import 'package:logger/logger.dart';
 
 import 'interfaces/IWebBloc.dart';
 
 abstract class BaseWebBloc<T extends IRepository, TEnum extends Object> extends BaseCommonBloc<T, TEnum, Element> implements IWebBloc<TEnum> {
-  BaseWebBloc({
+  static set defaultLogger(Logger? logger)
+    => BaseCommonBloc.defaultLogger = logger;
+
+  static Logger? get defaultLogger
+    => BaseCommonBloc.defaultLogger;
+    
+    BaseWebBloc({
     required super.repository,
   });
 }
